ad7d8437acea7459fba8e0b73667db99945cd68e
mexrona <tuichievamexrona@gmail.com>
2025-06-18 16:25:13 +0300

    Add redux

--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -1,7 +1,40 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, current} from "@reduxjs/toolkit";
 
 const initialState = {
-    posts: [],
+    list: [
+        {
+            id: 5,
+            title: "Post 5",
+            image: "https://i.artfile.ru/1600x1200_353220_[www.ArtFile.ru].jpg",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo esse, blanditiis eligendi, corporis sed libero quibusdam totam eveniet provident omnis, sapiente debitis facere consectetur tenetur officia aperiam quis delectus fuga.",
+        },
+        {
+            id: 4,
+            title: "Post 4",
+            image: "https://i.artfile.ru/1600x1200_353220_[www.ArtFile.ru].jpg",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo esse, blanditiis eligendi, corporis sed libero quibusdam totam eveniet provident omnis, sapiente debitis facere consectetur tenetur officia aperiam quis delectus fuga.",
+        },
+        {
+            id: 3,
+            title: "Post 3",
+            image: "https://i.artfile.ru/1600x1200_353220_[www.ArtFile.ru].jpg",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo esse, blanditiis eligendi, corporis sed libero quibusdam totam eveniet provident omnis, sapiente debitis facere consectetur tenetur officia aperiam quis delectus fuga.",
+        },
+        {
+            id: 2,
+            title: "Post 2",
+            image: "https://i.artfile.ru/1600x1200_353220_[www.ArtFile.ru].jpg",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo esse, blanditiis eligendi, corporis sed libero quibusdam totam eveniet provident omnis, sapiente debitis facere consectetur tenetur officia aperiam quis delectus fuga.",
+        },
+        {
+            id: 1,
+            title: "Post 1",
+            image: "https://i.artfile.ru/1600x1200_353220_[www.ArtFile.ru].jpg",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo esse, blanditiis eligendi, corporis sed libero quibusdam totam eveniet provident omnis, sapiente debitis facere consectetur tenetur officia aperiam quis delectus fuga.",
+        },
+    ],
+    postForView: null,
+    freshPosts: null,
 };
 
 export const postsSlice = createSlice({
@@ -9,13 +42,18 @@ export const postsSlice = createSlice({
     initialState,
     reducers: {
         setPosts: (state, action) => {
-            state.posts = action.payload;
+            state.list = action.payload;
         },
         editPost: (state, action) => {
             // edit post
         },
         getPost: (state, action) => {
-            // return post by id
+            state.postForView = state.list.find(
+                (item) => item.id === action.payload
+            );
+        },
+        getFreshPosts: (state) => {
+            state.freshPosts = state.list.slice(0, 3);
         },
         addPost: (state, action) => {
             // add new post by data
@@ -23,6 +61,7 @@ export const postsSlice = createSlice({
     },
 });
 
-export const {setPosts, editPost, getPost, addPost} = postsSlice.actions;
+export const {setPosts, editPost, getPost, getFreshPosts, addPost} =
+    postsSlice.actions;
 
 export default postsSlice.reducer;
